cc8566abf767caf6837547199e60cb8513bfa65f
bhattarai-pratham <prathammanibhattarai@proton.me>
2025-11-25 14:16:57 +1100

    chore: Bump version to 1.3.0 in app.config.js and app.json

--- app.config.js
+++ app.config.js
@@ -2,7 +2,7 @@ module.exports = {
   expo: {
     name: "Tracker",
     slug: "tracker-v2",
-    version: "1.2.1",
+    version: "1.3.0",
     orientation: "portrait",
     scheme: "trackerv2",
     userInterfaceStyle: "automatic",
